85cd1fd745c50d2b93123f84de44bbce58148770
Gihzx <145726882+Gihzx@users.noreply.github.com>
2025-03-04 18:22:20 -0300

    criando uma tag estilizada

---
+++ src/Components/TituloDinamico.js
@@ -0,0 +1,24 @@
+class TituloDinamico extends HTMLElement {
+  constructor() {
+    //primeira coisa a ser execultada
+    super(); //chamndo o construtor do HTMLElemnt
+
+    const shadow = this.attachShadow({ mode: "open" });
+
+    //base do component <h1>Gisele</h1>
+    const componentRoot = document.createElement("h1");
+    componentRoot.textContent = "Gisele";
+
+    //estilização do component
+    const style = document.createElement("style")
+    style.textContent= `
+    h1{
+    color: red
+    }
+    `
+    //enviar para o shadow
+    shadow.appendChild(componentRoot);
+    shadow.appendChild(style)
+  }
+}
+customElements.define("titulo-dinamico", TituloDinamico); //como ele vai se chamar/ o - é necessario para diferenciar das tag padrão
