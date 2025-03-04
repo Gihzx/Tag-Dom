71e0fe51530ca2d3b54f7432d9359d9ca4e2eb63
Gihzx <145726882+Gihzx@users.noreply.github.com>
2025-03-04 18:41:30 -0300

    ultilizando props

--- src/Components/TituloDinamico.js
+++ src/Components/TituloDinamico.js
@@ -7,18 +7,18 @@ class TituloDinamico extends HTMLElement {
 
     //base do component <h1>Gisele</h1>
     const componentRoot = document.createElement("h1");
-    componentRoot.textContent = "Gisele";
+    componentRoot.textContent = this.getAttribute("titulo");
 
     //estilização do component
-    const style = document.createElement("style")
-    style.textContent= `
+    const style = document.createElement("style");
+    style.textContent = `
     h1{
     color: red
     }
-    `
+    `;
     //enviar para o shadow
     shadow.appendChild(componentRoot);
-    shadow.appendChild(style)
+    shadow.appendChild(style);
   }
 }
 customElements.define("titulo-dinamico", TituloDinamico); //como ele vai se chamar/ o - é necessario para diferenciar das tag padrão
